13a932d95239e9593462ad9cd035d3f013b26e82
JoeCordingley <joewcordingley@gmail.com>
2020-11-13 16:15:20 +0000

    added extra slash endpoint

--- src/main/scala/com/martinsnyder/chatserver/ChatRoutes.scala
+++ src/main/scala/com/martinsnyder/chatserver/ChatRoutes.scala
@@ -20,8 +20,7 @@ import org.http4s.{HttpRoutes, MediaType, StaticFile}
 class ChatRoutes[F[_]: Sync: ContextShift](
     chatState: Ref[F, ChatState],
     queue: Queue[F, InputMessage],
-    topic: Topic[F, OutputMessage],
-    lastMessage: Option[InputMessage] = None
+    topic: Topic[F, OutputMessage]
 ) extends Http4sDsl[F] {
 
   private val blocker = {
@@ -61,6 +60,39 @@ class ChatRoutes[F[_]: Sync: ContextShift](
 
       // Bind a WebSocket connection for a user
       case GET -> Root / "ws" / userName =>
+        // Routes messages from our "topic" to a WebSocket
+        val toClient: Stream[F, WebSocketFrame.Text] =
+          topic
+            .subscribe(1000)
+            .filter(_.forUser(userName))
+            .map(msg => Text(msg.toString))
+
+        // Function that converts a stream of one type to another. Effectively an external "map" function
+        def processInput(wsfStream: Stream[F, WebSocketFrame]): Stream[F, Unit] = {
+          // Stream of initialization events for a user
+          val entryStream: Stream[F, InputMessage] = Stream.emits(Seq(EnterRoom(userName, InputMessage.DefaultRoomName)))
+
+          // Stream that transforms between raw text from the client and parsed InputMessage objects
+          val parsedWebSocketInput: Stream[F, InputMessage] =
+            wsfStream
+              .collect {
+                case Text(text, _) => InputMessage.parse(userName, text)
+
+                // Convert the terminal WebSocket event to a User disconnect message
+                case Close(_) => Disconnect(userName)
+              }
+
+          // Create a stream that has all of the user input sandwiched between the entry and disconnect messages
+          (entryStream ++ parsedWebSocketInput).through(queue.enqueue)
+        }
+
+        // WebSocketBuilder needs a "pipe" which is a type alias for a stream transformation function like processInput above
+        // This variable is not necessary to compile, but is included to clarify the exact type of Pipe.
+        val inputPipe: Pipe[F, WebSocketFrame, Unit] = processInput
+
+        // Build the WebSocket handler
+        WebSocketBuilder[F].build(toClient, inputPipe)
+      case GET -> Root / "ws" / userName / "" =>
         // Routes messages from our "topic" to a WebSocket
         val toClient: Stream[F, WebSocketFrame.Text] =
           topic
